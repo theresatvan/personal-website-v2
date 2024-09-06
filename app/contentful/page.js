2ed6b55b616081273e2e2cc02e5d17677aed1959
theresatvan <theresatvan@gmail.com>
2024-09-06 02:13:08 -0700

    Get assets from query

--- app/contentful/page.js
+++ app/contentful/page.js
@@ -1,15 +1,28 @@
 import client from '../../lib/api';
+import Image from 'next/image';
+import { BLOCKS } from '@contentful/rich-text-types';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const POST_QUERY = `
   query {
-    testPostCollection(where: { postId: 1 }) {
+    testPostCollection(where: { postId: 1 }, limit: 1) {
       items {
         postId
         title
         date
         body {
-          json
+          json 
+          links {
+            assets {
+              block {
+                sys {
+                  id
+                }
+                url
+                title
+              }
+            }
+          }
         }
       }
     }
@@ -22,12 +35,30 @@ export default async function Page() {
 
   const post = data.testPostCollection.items[0];
   const { title, date, body } = post;
+  const asset = body.links.assets.block[0];
+
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+        if (node.data.target.sys.id === asset.sys.id) {
+          return (
+            <Image
+              src={asset.url}
+              alt={asset.title || 'Image'}
+              width={500}
+              height={60}
+            />
+          );
+        }
+      },
+    },
+  };
 
   return (
     <div>
       <h1>{title}</h1>
       <p>{new Date(date).toLocaleDateString()}</p>
-      <div>{documentToReactComponents(body.json)}</div>
+      <div>{documentToReactComponents(body.json, options)}</div>
     </div>
   );
 }
